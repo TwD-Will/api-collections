63a57a76648699e4d572a2f5ccd1038bcf97be9f
TwD-WiLL <ismaelmusic1@hotmail.com>
2022-06-06 15:34:13 -0300

    add sorteBy and groupBy for listof

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,24 +1,31 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val joao = Funcionario("joao", salario = 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
-    val maria = Funcionario("Maria",4000.0)
+    val joao = Funcionario("joao",  2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "CLT")
+    val maria = Funcionario("Maria", 4000.0, "PJ")
 
     val funcionario = listOf(joao, pedro, maria)
 
-    funcionario.forEach{ println(it) }
+    funcionario.forEach { println(it) }
 
     println("---find para encontrar a variavel na lista----")
     println(funcionario.find { it.nome == "Maria" })
+
+    println("===-aplicando o sortedBy-===")
+    funcionario.sortedBy { it.salario }.forEach { println(it) }
+    println("--aplicando o groupBy--")
+    funcionario.groupBy { it.tipoContratacao }.forEach { println(it) }
+
 }
 
-data class  Funcionario(
+data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ) {
     override fun toString(): String =
-    """
+        """
         Nome:       $nome
         Salario:    $salario
     """.trimIndent()
