67440f92e7e518823d7ce2f0bf0aea5e847f8e89
TwD-WiLL <ismaelmusic1@hotmail.com>
2022-06-06 12:16:12 -0300

    dio new local to test

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
@@ -0,0 +1,29 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    for (valor in values){
+        println(valor)
+    }
+    println("---")
+    values.forEach {
+        println(it)
+    }
+    println("____")
+    for ( index in values.indices){
+        println(values[index])
+    }
+    println("===")
+    //ordenação do maior para o menor
+    values.sort()
+    for (valor in values){
+        println(valor)
+    }
+}
