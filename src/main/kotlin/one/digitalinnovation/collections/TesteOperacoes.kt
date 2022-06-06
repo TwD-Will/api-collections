057e11ac9d5a138a304effb65a550ec749c30265
TwD-WiLL <ismaelmusic1@hotmail.com>
2022-06-06 14:41:13 -0300

    add a count, to count between arrays, find to get specific value in, and  any to show a true or a false value

--- src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -6,12 +6,23 @@ fun main() {
     for (salario in salarios){
         println(salario)
     }
-    println("====")
+    println("== maior, menor e media de array==")
 
     println("Maior salário: ${salarios.maxOrNull()}")
     println("Menor salário: ${salarios.minOrNull()}")
     println("Média salarial: ${salarios.average()}")
-    println("===---===")
+    println("===- array maior que 2500--===")
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
     salariosMaiorQue2500.forEach{println(it)}
+
+    println("----add a count entre os arrays--==-=-")
+    println(salarios.count {it in 2000.0..5000.0})
+
+    println("===pegar valor especifico dentro do array====")
+    println(salarios.find {it == 2550.0})
+    println(salarios.find {it == 550.0})
+
+    println("---qualquer elemento dentro do array true or false===")
+    println(salarios.any{it == 1000.0})
+    println(salarios.any{it == 500.0})
 }
